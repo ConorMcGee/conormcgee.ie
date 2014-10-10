bdda2c8ceba5c50c0a3d8edf01f52eb14c67a996
mcgeeco <mcgeeco@tcd.ie>
2014-10-10 19:28:07 +0100

    added 404.html

--- conorblog/settings.py
+++ conorblog/settings.py
@@ -24,7 +24,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['.conormcgee.com', '.conormcgee.ie', '.conormcgee.me', ]
+ALLOWED_HOSTS = ['.conormcgee.com', '.conormcgee.ie', '.conormcgee.me', 'localhost']
 
 
 # Application definition
